c27c61190b81f23cf0bc21ff1891fe117670362b
Joao Lima <joaolimatech@gmail.com>
2025-05-28 00:18:01 -0300

    Adc comentários explicativos/To-do's

--- BookNow_KotlinApp2705/app/src/main/java/com/example/booknow/GeneroActivity.kt
+++ BookNow_KotlinApp2705/app/src/main/java/com/example/booknow/GeneroActivity.kt
@@ -12,6 +12,7 @@ class GeneroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_genero)
 
+// Referencia os botões da tela de escolha de gênero
         val btnFiccao = findViewById<Button>(R.id.btnFiccao)
         val btnComedia = findViewById<Button>(R.id.btnComedia)
         val btnBiografia = findViewById<Button>(R.id.btnBiografia)
@@ -21,15 +22,18 @@ class GeneroActivity : AppCompatActivity() {
         val btnHq = findViewById<Button>(R.id.btnHq)
         val btnCientifico = findViewById<Button>(R.id.btnCientifico)
 
+        //aplica a msm ação (de clique) pra todos os butões
         val buttons = listOf(btnFiccao, btnComedia, btnBiografia, btnSuspense, btnRomance, btnAcao, btnHq, btnCientifico)
 
+        //pra cada botao, define uma ação
         buttons.forEach { button ->
             button.setOnClickListener {
                 val genero = button.text.toString()
                 Toast.makeText(this, "Você escolheu $genero", Toast.LENGTH_SHORT).show()
 
-                // Aqui da pra abrir a próxima tela tipo: Lista de Livros
+                // Aqui da pra abrir a próxima tela tipo: Lista de Livros do genero escolhido
                 // startActivity(Intent(this, ListaLivrosActivity::class.java))
+                //pesquisar sobre  uma variavel intent pra abrir a tela de lista de livros e nao ficar fazendo varios layouts pra mesma coisa
             }
         }
     }
