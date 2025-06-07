8804889be124d9615cd4abae1fcb283b204a8c99
Victor-Diniz-Dev <victordinizdev@gmail.com>
2025-06-07 01:52:35 -0300

    Adiciona interface de comunicação com a API Flask usando Retrofit

---
+++ BookNow_KotlinApp2705/app/src/main/java/com/example/booknow/data/ApiService.kt
@@ -0,0 +1,35 @@
+package com.example.booknow.data
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface ApiService {
+
+    @Headers("Content-Type: application/json")
+    @POST("usuarios")
+    fun cadastrarUsuario(@Body usuario: Usuario): Call<ApiResponse>
+
+    @Headers("Content-Type: application/json")
+    @POST("login")
+    fun login(@Body loginRequest: LoginRequest): Call<ApiResponse>
+
+    companion object {
+        fun create(): ApiService {
+            return RetrofitClient.retrofit.create(ApiService::class.java)
+        }
+    }
+
+}
+
+data class LoginRequest(
+    val email: String,
+    val senha: String
+)
+
+data class ApiResponse(
+    val mensagem: String,
+    val nome: String? = null
+)
+
