fccbd85c0a49b32a0b3951d48030b665ec38f132
Joao Lima <joaolimatech@gmail.com>
2025-06-08 14:47:53 -0300

    Fix botao cadastro

--- BookNow_KotlinApp2705/app/src/main/java/com/example/booknow/SplashActivity.kt
+++ BookNow_KotlinApp2705/app/src/main/java/com/example/booknow/SplashActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.example.booknow.model.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
